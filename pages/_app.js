9fbc091a0f01cc6f349f8269e5359ad005d888e1
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-06-09 21:09:21 +0200

    Add g-tag back to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -12,6 +13,20 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       {asPath.includes('-') ? null : <Navbar />}
 
       <Component {...pageProps} />
