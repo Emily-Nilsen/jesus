9002047e3f5d9e8c5e5406a21e58a3eae72a1258
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-06-09 22:53:16 +0200

    Add person schema markup to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,28 @@ function classNames(...classes) {
 function MyApp({ Component, pageProps }) {
   const { locale, locales, defaultLocale, asPath } = useRouter();
 
+  const personStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Jesús Reina',
+    jobTitle: 'Principal Violin Professor',
+    email: 'info@jesusreina.es',
+    telephone: '+34 91 0182978',
+    url: 'https://www.jesusreina.es',
+    image:
+      'https://res.cloudinary.com/dt3k2apqd/image/upload/v1653712685/Galamian/professors/jesus-photo-01_loefpu.webp',
+    logo: 'https://res.cloudinary.com/dt3k2apqd/image/upload/v1654604664/Jes%C3%BAs%20Reina/SVGs/logo-path_usfoco.svg',
+    birthPlace: 'Málaga, Spain',
+    nationality: 'Spanish',
+    description: 'Official website of the Spanish violinist Jesús Reina.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/JSON-LD.svg',
+  };
+
   return (
     <div>
+      <script type="application/ld+json">
+        {JSON.stringify(personStructuredData)}
+      </script>
       {asPath.includes('-') ? null : <Navbar />}
       <Component {...pageProps} />
       <Footer />
