4b9805742763055f047633b55237cec794317de8
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-06-10 07:51:41 +0200

    Add scripts to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,28 +11,54 @@ function classNames(...classes) {
 function MyApp({ Component, pageProps }) {
   const { locale, locales, defaultLocale, asPath } = useRouter();
 
-  const personStructuredData = {
-    '@context': 'https://schema.org',
-    '@type': 'Person',
-    name: 'Jesús Reina',
-    jobTitle: 'Principal Violin Professor',
-    email: 'info@jesusreina.es',
-    telephone: '+34 91 0182978',
-    url: 'https://www.jesusreina.es',
-    image:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/v1653712685/Galamian/professors/jesus-photo-01_loefpu.webp',
-    logo: 'https://res.cloudinary.com/dt3k2apqd/image/upload/v1654604664/Jes%C3%BAs%20Reina/SVGs/logo-path_usfoco.svg',
-    birthPlace: 'Málaga, Spain',
-    nationality: 'Spanish',
-    description: 'Official website of the Spanish violinist Jesús Reina.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/JSON-LD.svg',
-  };
-
   return (
     <div>
-      <script type="application/ld+json">
-        {JSON.stringify(personStructuredData)}
-      </script>
+      {/* Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* Schema.org */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `
+        {
+          "@context": "https://schema.org",
+          "@type": "Person",
+          "birthPlace": "Málaga, Spain",
+          "nationality": "Spanish",
+          "description": "Official website of the Spanish violinist Jesús Reina.",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Madrid",
+            "addressRegion": "Madrid",
+            "postalCode": "28010",
+            "streetAddress": "Calle Santa Engracia, 17, 3° Ofc. 11"
+          },
+          "logo": "https://res.cloudinary.com/dt3k2apqd/image/upload/v1654604664/Jes%C3%BAs%20Reina/SVGs/logo-path_usfoco.svg", 
+          "email": "info@jesusreina.es",
+          "image": "https://res.cloudinary.com/dt3k2apqd/image/upload/v1653712685/Galamian/professors/jesus-photo-01_loefpu.webp",
+          "jobTitle": "Principal Violin Professor",
+          "name": "Jesús Reina",
+          "telephone": "+34 91 0182978",
+          "url": "https://www.jesusreina.es"
+        }
+    `,
+        }}
+      ></script>
       {asPath.includes('-') ? null : <Navbar />}
       <Component {...pageProps} />
       <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -11,25 +11,6 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-          {/* Schema.org */}
-
           <link
             rel="icon"
             href="https://res.cloudinary.com/dt3k2apqd/image/upload/v1654604664/Jes%C3%BAs%20Reina/SVGs/logo-path_usfoco.svg"
